00299e7ae47678a35fe575dc67a3496f9e9cb961
fox-on-nix <nix@nomail.com>
2026-01-06 10:41:53 +0100

    refactor(lazy-lock.json, lua/plugins/treesitter.lua): update dependencies and simplify treesitter configuration
    1. Updated `lazy-lock.json` to use the latest commit for `nvim-treesitter-textobjects`.
    2. Simplified the `treesitter.lua` file by removing redundant code and using a more concise setup method.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,7 @@
 -- lua/plugins/treesitter.lua
-return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = { enable = true },
-      ensure_installed = { "python", "c", "cpp", "bash" },
-    })
-  end,
-}
+
+local treesitter = require("nvim-treesitter")
+treesitter.setup({
+	highlight = { enable = true },
+	ensure_installed = { "python", "c", "cpp", "bash", "lua" },
+})
