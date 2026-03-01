016cedb38594e940ee9a62e3601f3e839a47ba56
fox-on-nix <nix@nomail.com>
2026-03-01 14:40:55 +0100

    Added '.gitignore'

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,27 +1,24 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
-vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.termguicolors = true
-vim.opt.errorformat = "%f:%l:%c: %m"
-
--- Netrw --
--- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.netrw_browse_split = 4 -- Open directories in a split
-vim.g.netrw_liststyle = 3 -- Show files and dirs with a tree view
 vim.g.netrw_altv = 1 -- Vertical split for :Vex
 vim.g.netrw_banner = 0 -- Hide banner
-vim.g.netrw_sort_sequence = "[\\/]*" -- Sort by time modified
+vim.g.netrw_browse_split = 4 -- Open directories in a split
 vim.g.netrw_hide = "" -- Show dotfiles
+vim.g.netrw_liststyle = 3 -- Show files and dirs with a tree view
+vim.g.netrw_sort_sequence = "[\\/]*" -- Sort by time modified
 vim.g.netrw_winsize = 25 -- Window size
+vim.opt.backup = false
+vim.opt.errorformat = "%f:%l:%c: %m"
+vim.opt.expandtab = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
+vim.opt.undofile = true
+vim.opt.wrap = false
